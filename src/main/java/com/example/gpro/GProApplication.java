b072dc92a5f8558835e09074eff9774fc33447b5
nishide_shoma <avanzarellc.tec@gmail.com>
2024-02-20 23:33:21 +0900

    First Commit

---
+++ src/main/java/com/example/gpro/GProApplication.java
@@ -0,0 +1,13 @@
+package com.example.gpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gpro/GProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
